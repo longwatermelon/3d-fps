1790b4b92cd592ca541d3b82cbd8cd8df3480cc4
longwatermelon <longwatermelon1@gmail.com>
2022-06-10 23:30:57 -0700

    name change

--- src/main.c
+++ src/main.c
@@ -15,7 +15,7 @@ int main(int argc, char **argv)
 
     audio_init();
 
-    SDL_Window *w = SDL_CreateWindow("Fps", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 800, 800, SDL_WINDOW_SHOWN);
+    SDL_Window *w = SDL_CreateWindow("Rasterize", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 800, 800, SDL_WINDOW_SHOWN);
     SDL_Renderer *r = SDL_CreateRenderer(w, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
 
     while (true)
